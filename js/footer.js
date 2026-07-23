540d2ed9e0cc572b614f6b1c4ffd4f7d9487b267
gabriele1211 <gabriele121127@gmail.com>
2026-07-24 01:52:23 +0200

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 (function () {
-  const version = window.APP_CONFIG?.APP_VERSION || "3.3";
+  const version = window.APP_CONFIG?.APP_VERSION || "3.3.1";
   document.querySelectorAll("[data-app-version]").forEach((element) => {
     element.textContent = version;
   });
